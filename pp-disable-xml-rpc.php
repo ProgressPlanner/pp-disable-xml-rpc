56a764030bf3ff91a80641b55f72e9e8291e10cf
Filip Ilic <ilic.filip@gmail.com>
2025-12-11 09:59:51 +0100

    Bump "Tested up to" version to 6.9

--- pp-disable-xml-rpc.php
+++ pp-disable-xml-rpc.php
@@ -8,6 +8,7 @@
  * Plugin URI:        https://progressplanner.com
  * Description:       A utility plugin to disable XML-RPC.
  * Requires at least: 6.6
+ * Tested up to:      6.9
  * Requires PHP:      7.4
  * Version:           1.6.1
  * Author:            Team Emilia Projects
